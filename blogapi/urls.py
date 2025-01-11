c070a1ee76e460e8d624536f59b7b00547286999
ObiomaIkpe <obiomaikpe1@gmail.com>
2025-01-11 16:02:48 +0100

    modifying the blog API endpoint

--- blogapi/urls.py
+++ blogapi/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("api/register_user/", views.register_user, name="register_user"),
-    path("api/create_blog/", views.create_blog, name="create_blog")
+    path("api/create_blog/", views.create_blog, name="create_blog"),
+    path("api/blog_list/", views.blog_list, name="blog_list"),
+    path("api/update_blog/<int:pk>/", views.update_blog, name="update_blog")
 ]
--- blogapi/views.py
+++ blogapi/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
+from .models import Blog
 # Create your views here.
 
 @api_view(["POST"])
@@ -22,3 +23,25 @@ def create_blog(request):
         serializer.save(author=user)
         return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET"])
+def blog_list(request):
+    blogs = Blog.objects.all()
+    serializer = BlogSerializer(blogs, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["PUT"])
+@permission_classes([IsAuthenticated])
+def update_blog(request, pk):
+    user = request.user
+    blog = Blog.objects.get(id = pk)
+    if blog.author != user:
+        return Response({"error": "you are not authorized to modify this blog"}, status=status.HTTP_403_FORBIDDEN)
+    serializer = BlogSerializer(blog, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
