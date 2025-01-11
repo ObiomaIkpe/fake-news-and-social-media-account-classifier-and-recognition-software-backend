c7fb0d1dd2e5abab92a5e0c1191ad7ed39d62d51
ObiomaIkpe <obiomaikpe1@gmail.com>
2025-01-11 11:50:51 +0100

    creating custom blog model

--- FN_SaaS/urls.py
+++ FN_SaaS/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
@@ -24,3 +27,5 @@ urlpatterns = [
     path('api/token_refresh/', TokenRefreshView .as_view(), name='token_refresh'),
     path("", include("blogapi.urls"))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
